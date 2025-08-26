4782210b024c0203ef6941a83b208ca1c5dc7f57
Ahmad Pijar <pijar1806@gmail.com>
2025-08-26 23:10:39 +0700

    add new Project and remake Description on my previous project

--- src/component/data/projectData.jsx
+++ src/component/data/projectData.jsx
@@ -1,23 +1,31 @@
-import projectInventory from "../../../public/INVENTORY-APP.pdf";
 const images = import.meta.glob("../../assets/*.png", { eager: true });
 
 const contentData = [
   {
     id: 1,
     image: images["../../assets/portofolio-website.png"].default,
-    title: "Personal Portofolio website",
+    title: "Portfolio Website – React & Tailwind Project",
     description:
-      "Portfolio Website is a dynamically designed platform built with React and Tailwind CSS, powered by Vite. It serves as both a personal introduction and professional showcase, featuring skills, projects, contact, and a downloadable CV. This site highlights a broad range of my abilities — presented through interactive components and a clean responsive design. The site reflects not only what I’ve done, but also my dedication to continuous learning and adaptable growth.",
+      "A responsive personal site built with React, Tailwind CSS, and Vite. It presents my skills, projects, CV, and contact info in a clean, interactive layout. Designed to highlight both technical ability and professional growth, it adapts smoothly across devices.",
     link: "https://ahmadpijar.github.io/my-portofolio",
     buttonTitle: "Visit Website",
   },
   {
     id: 2,
     image: images["../../assets/Inventory-app.png"].default,
-    title: "Inventory Management App",
+    title: "Inventory Management App – Excel VBA Project",
     description:
-      "Inventory Management App is a macro-enabled Excel tool built with VBA to track warehouse operations. It automates the recording of inbound and outbound transactions, dynamically calculates stock balances, and generates reports at the click of a button. Designed for small businesses or teams without access to advanced ERP systems, this project highlights my ability to combine administrative experience with technical problem-solving — delivering practical, user-friendly solutions.",
-    link: projectInventory,
+      "A macro-enabled Excel tool created to manage warehouse stock. It records inbound and outbound items, calculates real-time balances, and uses VBA for automation and reporting. This project reflects my ability to turn hands-on warehouse experience into an efficient, user-friendly digital system.",
+    link: "https://docs.google.com/presentation/d/1DfDC86xJ-q8ggxqzkwKNZ8PTxMPQngVuWrmqmoNA_FU/edit?usp=drive_link",
+    buttonTitle: "View Presentation",
+  },
+  {
+    id: 3,
+    image: images["../../assets/Project3-Payroll_app.png"].default,
+    title: "Payroll Management App – Excel VBA Project",
+    description:
+      "This project is a fully functional payroll and attendance management tool built entirely in Microsoft Excel, enhanced with VBA automation. It’s designed to simplify HR and administrative tasks for small to medium-sized teams by centralizing employee data, timesheets, and payroll calculations in one place.",
+    link: "https://docs.google.com/presentation/d/16Xv_SsUE1ih-1DsLY4IFX0lwgHeaGEXO1iW4fEK3W3c/edit?usp=sharing",
     buttonTitle: "View Presentation",
   },
 ];
